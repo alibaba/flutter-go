d0403c515ed3b6193ccfeb87191ee56e8fc00cb6
sanfan.hx <sanfan.hx@alibaba-inc.com>
2019-01-18 16:37:17 +0800

    add(CupertinoSegmentedControl demo && CupertinoSlider demo)
    add(CupertinoSegmentedControl demo && CupertinoSlider demo)

--- lib/widgets/themes/Cupertino/index.dart
+++ lib/widgets/themes/Cupertino/index.dart
@@ -1,10 +1,22 @@
 import "package:flutter/material.dart";
 import '../../../model/widget.dart';
 import './CupertinoScrollbar/index.dart' as CupertinoScrollbar;
+import './CupertinoSlider/index.dart' as CupertinoSlider;
+import './CupertinoSegmentedControl/index.dart' as CupertinoSegmentedControl;
 List<WidgetPoint> widgetPoints = [
   WidgetPoint(
     name: 'CupertinoScrollbar',
     routerName: CupertinoScrollbar.Demo.routeName,
     buildRouter: (BuildContext context) => CupertinoScrollbar.Demo(),
+  ),
+  WidgetPoint(
+    name: 'CupertinoSlider',
+    routerName: CupertinoSlider.Demo.routeName,
+    buildRouter: (BuildContext context) => CupertinoSlider.Demo(),
+  ),
+  WidgetPoint(
+    name: 'CupertinoSegmentedControl',
+    routerName: CupertinoSegmentedControl.Demo.routeName,
+    buildRouter: (BuildContext context) => CupertinoSegmentedControl.Demo(),
   )
 ];
