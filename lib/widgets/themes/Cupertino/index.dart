d840088144c7053ef283912c712a1e80120a6505
sanfan.hx <sanfan.hx@alibaba-inc.com>
2019-01-20 22:18:44 +0800

    feat(CupertinoSwitch demo):

--- lib/widgets/themes/Cupertino/index.dart
+++ lib/widgets/themes/Cupertino/index.dart
@@ -4,6 +4,7 @@ import './CupertinoScrollbar/index.dart' as CupertinoScrollbar;
 import './CupertinoSlider/index.dart' as CupertinoSlider;
 import './CupertinoSegmentedControl/index.dart' as CupertinoSegmentedControl;
 import './CupertinoSliverNavigationBar/index.dart' as CupertinoSliverNavigationBar;
+import './CupertinoSwitch/index.dart' as CupertinoSwitch;
 List<WidgetPoint> widgetPoints = [
   WidgetPoint(
     name: 'CupertinoScrollbar',
@@ -29,5 +30,10 @@ List<WidgetPoint> widgetPoints = [
     name: 'CupertinoSliverRefreshControl',
     routerName: CupertinoSliverNavigationBar.Demo.routeName,
     buildRouter: (BuildContext context) => CupertinoSliverNavigationBar.Demo(),
+  ),
+  WidgetPoint(
+    name: 'CupertinoSwitch',
+    routerName: CupertinoSwitch.Demo.routeName,
+    buildRouter: (BuildContext context) => CupertinoSwitch.Demo(),
   ),
 ];
