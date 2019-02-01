71b56286e11724017ebee808ae0ddc0006b9dfb6
sanlu <xiaojia.dxj@alibaba-inc.com>
2019-02-01 17:50:03 +0800

    feat:add android splash

--- android/app/src/main/java/com/example/flutterrookiebook/MainActivity.java
+++ android/app/src/main/java/com/example/flutterrookiebook/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+//      setContentView(R.layout.activity_main);
     GeneratedPluginRegistrant.registerWith(this);
   }
 }
