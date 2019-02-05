63b9dc694e1253f41edd88acdf4a3279b49d8942
sanfan.hx <sanfan.hx@alibaba-inc.com>
2019-02-05 22:28:50 +0800

    feat(add city picker demo):

--- lib/widgets/components/Pick/CityPIcker/demo.dart
+++ lib/widgets/components/Pick/CityPIcker/demo.dart
@@ -8,14 +8,37 @@
 //
 
 import 'package:flutter/material.dart';
+import 'package:city_pickers/city_pickers.dart';
 
-class Demo extends StatefulWidget {
+class CityPickerDemo extends StatefulWidget {
   _Demo createState() => _Demo();
 }
 
-class _Demo extends State<Demo> {
+class _Demo extends State<CityPickerDemo> {
+  String _result;
+  show(context) async {
+    Result temp  = await CityPickers.showCityPicker(
+      context: context,
+//      locationCode: '640221',
+      height: 400,
+    );
+    setState(() {
+      _result = "${temp.toString()}";
+    });
+  }
 
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+        child: Column(
+          children: <Widget>[
+            Text("result: ${_result.toString()}"),
+            RaisedButton(
+              onPressed: () {this.show(context);
+              },
+              child: Text("select"),
+            ),
+          ],
+        )
+    );
   }
 }
--- lib/widgets/components/Pick/CityPIcker/index.dart
+++ lib/widgets/components/Pick/CityPIcker/index.dart
@@ -8,14 +8,36 @@
 //
 
 import 'package:flutter/material.dart';
-
+import 'package:flutter_go/components/widget_demo.dart';
+import 'demo.dart';
+const String content0 = '''
+### **简介**
+> 显示中国的省市县地区
+通过函数 **showCityPicker** 调用, 在容器窗口上弹出遮罩层
+''';
+const String content1 = '''
+### **基本用法**
+- locationCode 初始化地区选择
+- height 弹出层高度
+- context 上下文对象
+''';
 class Demo extends StatefulWidget {
+  static const String routeName = '/components/Pick/CityPicker';
   _Demo createState() => _Demo();
 }
 
 class _Demo extends State<Demo> {
 
   Widget build(BuildContext context) {
-    return Container();
+    return WidgetDemo(
+      title: 'CityPicker',
+      codeUrl: '',
+      docUrl: '',
+      contentList: [
+        content0,
+        content1,
+        CityPickerDemo()
+      ],
+    );
   }
 }
