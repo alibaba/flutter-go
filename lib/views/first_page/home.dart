9ccf7649277dd4e5ca80f2c64c9ab06475d4f571
xj.deng <xiaojia22326@163.com>
2019-05-08 10:09:13 +0800

    targ:ip 1.0.6

--- lib/views/first_page/home.dart
+++ lib/views/first_page/home.dart
@@ -1,4 +1,3 @@
-
 /// Created with Android Studio.
 /// User: 三帆
 /// Date: 16/01/2019
@@ -6,11 +5,9 @@
 /// email: sanfan.hx@alibaba-inc.com
 /// target:  app首页
 
-
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_go/utils/shared_preferences.dart';
-import 'package:flutter_go/views/first_page/first_page.dart';
 import 'package:flutter_go/views/widget_page/widget_page.dart';
 import 'package:flutter_go/views/welcome_page/fourth_page.dart';
 import 'package:flutter_go/views/collection_page/collection_page.dart';
@@ -36,41 +33,41 @@ class _MyHomePageState extends State<AppPage>
   SpUtil sp;
   WidgetControlModel widgetControl = new WidgetControlModel();
   SearchHistoryList searchHistoryList;
-  TabController controller;
   bool isSearch = false;
-  String data = '无';
-  String data2ThirdPage = '这是传给ThirdPage的值';
   String appBarTitle = tabData[0]['text'];
+  List<Widget> list = List();
+  int _currentIndex = 0;
   static List tabData = [
-    {'text': '业界动态', 'icon': new Icon(Icons.language)},
-    {'text': 'WIDGET', 'icon': new Icon(Icons.extension)},
-    {'text': '组件收藏', 'icon': new Icon(Icons.favorite)},
-    {'text': '关于手册', 'icon': new Icon(Icons.import_contacts)}
+//    {'text': '业界动态', 'icon': Icon(Icons.language)},
+    {'text': 'WIDGET', 'icon': Icon(Icons.extension)},
+    {'text': '组件收藏', 'icon': Icon(Icons.favorite)},
+    {'text': '关于手册', 'icon': Icon(Icons.import_contacts)},
   ];
 
-  List<Widget> myTabs = [];
+  List<BottomNavigationBarItem> myTabs = [];
 
   @override
   void initState() {
     super.initState();
-
     initSearchHistory();
-    controller = new TabController(
-        initialIndex: 0, vsync: this, length: 4); // 这里的length 决定有多少个底导 submenus
     for (int i = 0; i < tabData.length; i++) {
-      myTabs.add(new Tab(text: tabData[i]['text'], icon: tabData[i]['icon']));
+      myTabs.add(BottomNavigationBarItem(
+        icon: tabData[i]['icon'],
+        title: Text(
+          tabData[i]['text'],
+        ),
+      ));
     }
-    controller.addListener(() {
-      if (controller.indexIsChanging) {
-        _onTabChange();
-      }
-    });
-    Application.controller = controller;
+    list
+
+//      ..add(FirstPage())
+      ..add(WidgetPage(Provider.db))
+      ..add(CollectionPage())
+      ..add(FourthPage());
   }
 
   @override
   void dispose() {
-    controller.dispose();
     super.dispose();
   }
 
@@ -92,7 +89,9 @@ class _MyHomePageState extends State<AppPage>
     });
     searchHistoryList
         .add(SearchHistory(name: targetName, targetRouter: targetRouter));
-    print("searchHistoryList ${searchHistoryList.toString()}");
+    print("searchHistoryList1 ${searchHistoryList.toString()}");
+    print("searchHistoryList2 ${targetRouter}");
+    print("searchHistoryList3 ${widgetPoint.name}");
     Application.router.navigateTo(context, "$targetRouter");
   }
 
@@ -100,16 +99,15 @@ class _MyHomePageState extends State<AppPage>
     return new SearchInput((value) async {
       if (value != '') {
         List<WidgetPoint> list = await widgetControl.search(value);
-
         return list
             .map((item) => new MaterialSearchResult<String>(
-                  value: item.name,
-                  icon: WidgetName2Icon.icons[item.name] ?? null,
-                  text: 'widget',
-                  onTap: () {
-                    onWidgetTap(item, context);
-                  },
-                ))
+          value: item.name,
+          icon: WidgetName2Icon.icons[item.name] ?? null,
+          text: 'widget',
+          onTap: () {
+            onWidgetTap(item, context);
+          },
+        ))
             .toList();
       } else {
         return null;
@@ -117,54 +115,31 @@ class _MyHomePageState extends State<AppPage>
     }, (value) {}, () {});
   }
 
+
   @override
   Widget build(BuildContext context) {
-    var db = Provider.db;
-
     return new Scaffold(
-      appBar: new AppBar(title: buildSearchInput(context)),
-      body: new TabBarView(controller: controller, children: <Widget>[
-        new FirstPage(),
-        new WidgetPage(db),
-        new CollectionPage(),
-        FourthPage()
-      ]),
-      bottomNavigationBar: Material(
-        color: const Color(0xFFF0EEEF), //底部导航栏主题颜色
-        child: SafeArea(
-          child: Container(
-            height: 65.0,
-            decoration: BoxDecoration(
-              color: const Color(0xFFF0F0F0),
-              boxShadow: <BoxShadow>[
-                BoxShadow(
-                  color: const Color(0xFFd0d0d0),
-                  blurRadius: 3.0,
-                  spreadRadius: 2.0,
-                  offset: Offset(-1.0, -1.0),
-                ),
-              ],
-            ),
-            child: TabBar(
-                controller: controller,
-                indicatorColor: Theme.of(context).primaryColor,
-                //tab标签的下划线颜色
-                // labelColor: const Color(0xFF000000),
-                indicatorWeight: 3.0,
-                labelColor: Theme.of(context).primaryColor,
-                unselectedLabelColor: const Color(0xFF8E8E8E),
-                tabs: myTabs),
-          ),
-        ),
+      appBar: AppBar(title: buildSearchInput(context)),
+      body: list[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: myTabs,
+        //高亮  被点击高亮
+        currentIndex: _currentIndex,
+        //修改 页面
+        onTap: _ItemTapped,
+        //shifting :按钮点击移动效果
+        //fixed：固定
+        type: BottomNavigationBarType.fixed,
+
+        fixedColor: Color(0xFFC91B3A),
       ),
     );
   }
 
-  void _onTabChange() {
-    if (this.mounted) {
-      this.setState(() {
-        appBarTitle = tabData[controller.index]['text'];
-      });
-    }
+  void _ItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+      appBarTitle = tabData[index]['text'];
+    });
   }
 }
