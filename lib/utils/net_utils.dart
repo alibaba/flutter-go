d66f2985122a45d7a2deadcdc892d1eef1d424cd
xj.deng <xiaojia22326@163.com>
2019-05-23 11:18:46 +0800

    test

--- lib/utils/net_utils.dart
+++ lib/utils/net_utils.dart
@@ -8,7 +8,12 @@ var dio = new Dio();
 class NetUtils {
   
   static Future get(String url,{Map<String,dynamic> params}) async{
-     var response = await dio.get(url, data: params);
+     var response ;
+     if (params != null) {
+       response = await dio.get(url, queryParameters: params);
+     } else {
+       response = await dio.get(url);
+     }
     return  response.data;
   }
 
