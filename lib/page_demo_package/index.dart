7009204fbee309a27ab3a6867889135c7c438df1
sanfan.hx <sanfan.hx@alibaba-inc.com>
2019-06-11 19:28:21 +0800

    加入初始化demo

--- lib/page_demo_package/index.dart
+++ lib/page_demo_package/index.dart
@@ -1,3 +1,4 @@
+import 'demoName_yourName_1a29aa8e_32ae_4241_9c8a_5c9e1f92b096/index.dart' as StandardDemo_demoName_1a29aa8e_32ae_4241_9c8a_5c9e1f92b096;
 var demoObjects = { 
-
+  '1a29aa8e_32ae_4241_9c8a_5c9e1f92b096': StandardDemo_demoName_1a29aa8e_32ae_4241_9c8a_5c9e1f92b096.demoWidgets
 };
