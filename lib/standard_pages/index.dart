039aa187cc4816403daa5d3b4cc0f9dd75e9ff78
sanfan.hx <sanfan.hx@alibaba-inc.com>
2019-06-11 19:28:49 +0800

    加入标准page

--- lib/standard_pages/index.dart
+++ lib/standard_pages/index.dart
@@ -1,9 +1,9 @@
-import 'page_snfan_facca78e_32ae_4241_9c8a_5c9e1f92b096/index.dart' as StandardPage_page_facca78e_32ae_4241_9c8a_5c9e1f92b096;
+import 'standard_sanfan_ee4feb8e_32ae_4241_9c8a_5c9e1f92b096/index.dart' as StandardPage_standard_ee4feb8e_32ae_4241_9c8a_5c9e1f92b096;
 class StandardPages {
   Map<String, String> standardPages;
   Map<String, String> getPages() {
     return {
-			'facca78e_32ae_4241_9c8a_5c9e1f92b096': StandardPage_page_facca78e_32ae_4241_9c8a_5c9e1f92b096.getMd()
+			'ee4feb8e_32ae_4241_9c8a_5c9e1f92b096': StandardPage_standard_ee4feb8e_32ae_4241_9c8a_5c9e1f92b096.getMd()
     };
   }
 }
