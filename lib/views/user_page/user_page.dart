e8475c77153a83fe3843ef1ea2149baf75da1bf9
yifeng.yl <yifeng.yl@alibaba-inc.com>
2019-08-08 19:41:07 +0800

    bottomBar 添加个人中心

---
+++ lib/views/user_page/user_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_go/model/user_info.dart';
+import 'package:flutter_go/views/first_page/drawer_page.dart';
+
+class UserPage extends StatefulWidget {
+  final UserInformation userInfo;
+
+  UserPage({Key key, this.userInfo}) : super(key: key);
+
+  @override
+  _UserPageState createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  @override
+  Widget build(BuildContext context) {
+    print(widget.userInfo);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('个人中心'),
+      ),
+      body: Container(
+        child: DrawerPage(
+          userInfo: widget.userInfo,
+        ),
+      ),
+    );
+  }
+}
