64513b59c1c387c7f4ad0695c7b0f03e0ffba1e5
ryan <zhu.yan@alibaba-inc.com>
2019-08-13 20:35:36 +0800

    Add:创建 flutter go web 版

--- lib/event/event_model.dart
+++
@@ -1,7 +0,0 @@
-class CollectionEvent{
-  final String widgetName;
-  final String router;
-  final bool isRemove;
-  // token uid...
-  CollectionEvent(this.widgetName,this.router,this.isRemove);
-}
