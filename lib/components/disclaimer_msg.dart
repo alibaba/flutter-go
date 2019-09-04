524864ed71632c2547764af3fe7eafb62562c29e
meetqy <37372842@qq.com>
2019-09-04 13:18:08 +0800

    bug： 修复免责声明Dialog 切换“不再自动提示“ checkbox 有非常明显的刷新闪烁

--- lib/components/disclaimer_msg.dart
+++ lib/components/disclaimer_msg.dart
@@ -26,8 +26,7 @@ class DisclaimerMsg extends StatefulWidget {
 class DisclaimerMsgState extends State<DisclaimerMsg> {
   Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   Future<bool> _unKnow;
-  var _valBool = false;
-  var _readed = false;
+  bool _getReaded = false;
 
   //SharedPreferences 存储结果
   void refs(bool value) async {
@@ -50,125 +49,143 @@ class DisclaimerMsgState extends State<DisclaimerMsg> {
       return (prefs.getBool('disclaimer::Boolean') ?? false);
     });
     _unKnow.then((bool value) {
-      _valBool = value;
-      _readed = value;
+      // 第一次需要自动弹出免费声明
+      if(!value) {
+        showAlertDialog(context, 
+          readedFn: (val) {
+            setState(() {
+              _getReaded = val;
+            });
+            refs(val);
+          }
+        );
+      } else {
+        setState(() {
+          _getReaded = value;
+        });
+      }
+      
     });
   }
 
-  void showAlertDialog(BuildContext context) {
+  /// @param readedFn 获取是否已读的回到函数
+  /// showDialog中的context和外面的context不一致，不能使用setState
+  /// showDialog中只控制checkBox的值
+  showAlertDialog(BuildContext context, {
+    @required Function readedFn,
+  }) {
+    bool _valBool = false;
+    bool _readed = _getReaded;
     showDialog<void>(
       context: context,
       barrierDismissible: false, // user must tap button!
       builder: (BuildContext context) {
-        return AlertDialog(
-          //title: Text('免责声明'),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                Container(
-                    padding: EdgeInsets.fromLTRB(5.0, 5.0, 10.0, 10.0),
-                    //width: 100,
-                    height: 35,
-                    child: Text('免责声明',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.w700)),
-                    decoration: BoxDecoration(
-                      //color: Colors.blue,
-                      image: DecorationImage(
-                          fit: BoxFit.fitWidth,
-                          image: AssetImage('assets/images/paimaiLogo.png')),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10.0),
-                      ),
-                      //alignment: Alignment.bottomRight,
-                    )),
-                SizedBox(height: 20),
-                Text(disclaimerText1),
-                Text(disclaimerText2),
-              ],
-            ),
-          ),
-          shape: RoundedRectangleBorder(
-              borderRadius: new BorderRadius.circular(20.0)), // 圆角
+        return StatefulBuilder(
+          builder: (context, state) {
+            return AlertDialog(
+              //title: Text('免责声明'),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: <Widget>[
+                    Container(
+                        padding: EdgeInsets.fromLTRB(5.0, 5.0, 10.0, 10.0),
+                        //width: 100,
+                        height: 35,
+                        child: Text('免责声明',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w700)),
+                        decoration: BoxDecoration(
+                          //color: Colors.blue,
+                          image: DecorationImage(
+                              fit: BoxFit.fitWidth,
+                              image: AssetImage('assets/images/paimaiLogo.png')),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10.0),
+                          ),
+                          //alignment: Alignment.bottomRight,
+                        )),
+                    SizedBox(height: 20),
+                    Text(disclaimerText1),
+                    Text(disclaimerText2),
+                  ],
+                ),
+              ),
+              shape: RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(20.0)), // 圆角
 
-          actions: <Widget>[
-            new Container(
-              width: 250,
-              child: _create(),
-            )
-          ],
+              actions: <Widget>[
+                new Container(
+                  width: 250,
+                  child: _readed ? Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      FlatButton(
+                        padding: EdgeInsets.symmetric(horizontal: 20.0),
+                        child: Text('已阅读知晓',
+                            style: TextStyle(fontSize: 16, color: Colors.white)),
+                        //可点击
+                        color: Theme.of(context).primaryColor,
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      )
+                    ],
+                  ) : Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  //crossAxisAlignment:CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Checkbox(
+                          activeColor: Theme.of(context).primaryColor,
+                          tristate: false,
+                          value: _valBool,
+                          onChanged: (bool bol) {
+                            state(() {
+                              _valBool = bol;
+                            });
+                          }
+                        ),
+                        Text('不再自动提示', style: TextStyle(fontSize: 14)),
+                      ],
+                    ),
+                    FlatButton(
+                      child: Text('知道了',
+                      style: TextStyle(fontSize: 16, color: Colors.white)),
+                      //可点击
+                      color: _valBool
+                          ? Theme.of(context).primaryColor
+                          : Theme.of(context).primaryColor.withAlpha(800),
+                      onPressed: () {
+                        if (_valBool) {
+                          // _valBool 在此处为true 不可能为false，
+                          // 所以readedFn返回的参数是true，代表已经阅读
+                          readedFn(_valBool);
+                          Navigator.of(context).pop(() {                            
+                            state(() {
+                              _readed = _valBool;
+                            });
+                          });
+                        }
+                      },
+                    ),
+                  ])
+                )
+              ],
+            );
+          },
         );
       },
     );
   }
 
-  Row _create() {
-    //已读
-    if (_readed) {
-      return Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          FlatButton(
-            padding: EdgeInsets.symmetric(horizontal: 20.0),
-            child: Text('已阅读知晓',
-                style: TextStyle(fontSize: 16, color: Colors.white)),
-            //可点击
-            color: Theme.of(context).primaryColor,
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-          SizedBox(
-            width: 10.0,
-          )
-        ],
-      );
-    }
-
-    //第一次读取
-    return Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
-        //crossAxisAlignment:CrossAxisAlignment.start,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Checkbox(
-                  activeColor: Theme.of(context).primaryColor,
-                  tristate: false,
-                  value: _valBool,
-                  onChanged: (bool bol) {
-                    if(mounted) {
-                      setState(() {
-                        _valBool = bol;
-                      });
-                    }
-                    Navigator.of(context).pop(); // here I pop to avoid multiple Dialogs
-                    showAlertDialog(context); //here i call the same function
-                  }),
-              Text('不再自动提示', style: TextStyle(fontSize: 14)),
-            ],
-          ),
-          FlatButton(
-            child: Text('知道了',
-                style: TextStyle(fontSize: 16, color: Colors.white)),
-            //可点击
-            color: _valBool
-                ? Theme.of(context).primaryColor
-                : Theme.of(context).primaryColor.withAlpha(800),
-            onPressed: () {
-              // if (_valBool) {
-              refs(_valBool);
-              Navigator.of(context).pop();
-              // }
-            },
-          ),
-        ]);
-  }
-
   Widget build(BuildContext context) {
     return GestureDetector(
         onTap: () {
-          showAlertDialog(context);
+          showAlertDialog(context, readedFn: (){});
         },
         child: Stack(
           //alignment: const Alignment(1.6, 1.6),
--- lib/views/first_page/first_page.dart
+++ lib/views/first_page/first_page.dart
@@ -18,35 +18,11 @@ class FirstPage extends StatefulWidget {
 }
 
 class FirstPageState extends State<FirstPage> with AutomaticKeepAliveClientMixin{
-  Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
-  Future<bool> _unKnow;
   GlobalKey<DisclaimerMsgState> key;
 
   @override
   bool get wantKeepAlive => true;
 
-
- @override
-  void initState() {
-    super.initState();
-    if (key == null) {
-       key = GlobalKey<DisclaimerMsgState>();
-       // key = const Key('__RIKEY1__');
-      //获取sharePre
-       _unKnow = _prefs.then((SharedPreferences prefs) {
-         return (prefs.getBool('disclaimer::Boolean') ?? false);
-       });
-
-      /// 判断是否需要弹出免责声明,已经勾选过不在显示,就不会主动弹
-      _unKnow.then((bool value) {
-         new Future.delayed(const Duration(seconds: 1),(){
-           if (!value && key.currentState is DisclaimerMsgState && key.currentState.showAlertDialog is Function) {
-             key.currentState.showAlertDialog(context);
-           }
-         });
-      });
-    }
-  }
 
 
   Future<Map> getIndexListData([Map<String, dynamic> params]) async {
